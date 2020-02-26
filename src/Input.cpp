985bf3f0ff6015f35e1140147825121160a822ed
Sergi Garcia <sergi.garciab@e-campus.uab.cat>
2020-02-26 23:09:57 +0100

    mouse button + pos handling

--- src/Input.cpp
+++ src/Input.cpp
@@ -3,7 +3,11 @@
 
 Input::Input(){
     std::memset(pressed_keys, 0, GLFW_KEY_LAST);
+    std::memset(pressed_mbuttons, 0, GLFW_MOUSE_BUTTON_LAST + 1);
     m_num_pressed_keys = 0;
+    m_mouse_posx = 0;
+    m_mouse_posy = 0;
+    m_mouse_moved = false;
 }
 
 
@@ -27,12 +31,52 @@ void Input::onKeyboardInput(int key, int scancode, int action, int mods){
 }
 
 
+void Input::onMouseButton(int button, int action, int mods){
+    UNUSED(mods);
+
+    if(button == GLFW_KEY_UNKNOWN) return;
+    if(action == GLFW_PRESS){
+        pressed_mbuttons[button] = true;
+    }
+    else{
+        pressed_mbuttons[button] = false;
+    }
+}
+
+
+void Input::onMouseButtonPos(double posx, double posy){
+    m_mouse_moved = true;
+    m_mouse_posx = posx;
+    m_mouse_posy = posy;
+}
+
+
 void Input::update(){
-    //key_pressed = false;
+    m_mouse_moved = false;
+    glfwPollEvents();
 }
 
 
 bool Input::keyboardPressed() const{
     return (m_num_pressed_keys > 0);
+}
+
+
+bool Input::mButtonPressed() const{
+    for(int i = 0; i < GLFW_MOUSE_BUTTON_LAST + 1; i++)
+        if(pressed_mbuttons[i])
+            return true;
+    return false;
+}
+
+
+bool Input::mouseMoved() const{
+    return m_mouse_moved;
+}
+
+
+void Input::getMousePos(double& posx, double& posy) const{
+    posx = m_mouse_posx;
+    posy = m_mouse_posy;
 }
 
--- src/Input.hpp
+++ src/Input.hpp
@@ -12,16 +12,27 @@
 class Input{
     private:
         int m_num_pressed_keys;
+        int m_num_pressed_mbuttons;
+        double m_mouse_posx, m_mouse_posy;
+        bool m_mouse_moved = false;
     public:
         bool pressed_keys[GLFW_KEY_LAST];
+        bool pressed_mbuttons[GLFW_MOUSE_BUTTON_LAST + 1];
 
         Input();
         //Input();
         ~Input(); 
 
         void update();
+        
         bool keyboardPressed() const;
+        bool mButtonPressed() const;
+        void getMousePos(double& posx, double& posy) const;
+        bool mouseMoved() const;
+
         void onKeyboardInput(int key, int scancode, int action, int mods);
+        void onMouseButton(int button, int action, int mods);
+        void onMouseButtonPos(double posx, double posy);
 };
 
 
