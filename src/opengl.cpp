9df99efef39f582126ca49e81bb06aac5fed3f49
Sergi Garcia <sergi.garciab@e-campus.uab.cat>
2020-03-07 14:00:10 +0100

    added fps counter

--- src/opengl.cpp
+++ src/opengl.cpp
@@ -72,6 +72,7 @@ int main(){
 
 	////////////////// text testing /////////////////
 	Text2D text2(gl_width, gl_height, color{1.0, 1.0, 0.0}, 256, "../data/fonts/Liberastika-Regular.ttf", 15);
+	Text2D text_fps(gl_width, gl_height, color{1.0, 1.0, 0.0}, 256, "../data/fonts/Liberastika-Regular.ttf", 15);
 	Text2D* text;
 	debug_info_box(&text, gl_width, gl_height);
 
@@ -92,9 +93,9 @@ int main(){
 	glClearColor(0.2, 0.2, 0.2, 1.0);
 
 	float rotation_angle = -90.f;
-	mat4 loc[10];
+	mat4 loc[100];
 	mat4 rotation = rotate_x_deg(identity_mat4(), rotation_angle);
-	for(int i = 0; i < 10; i++){
+	for(int i = 0; i < 100; i++){
 		loc[i] = translate(identity_mat4(), math::vec3(0., 0., i*std::sin(i*0.75)));
 		loc[i] = translate(loc[i], math::vec3(i*std::cos(i*0.75), 0., 0.)) * rotation;
 	}
@@ -122,7 +123,7 @@ int main(){
 		glActiveTexture(GL_TEXTURE0);
     	glBindTexture(GL_TEXTURE_2D, tex_id);
 		int num_rendered = 0;
-		for(int i=0; i < 10; i++){
+		for(int i=0; i < 100; i++){
 			//if(frustum.checkSphere(math::vec3(loc[i].m[12], loc[i].m[13], loc[i].m[14]), 2*0.994406)){
 			if(frustum.checkBox(aabb_duck.vert, loc[i])){
 				num_rendered += 1;
@@ -141,18 +142,26 @@ int main(){
 		mbstowcs(buffer, oss2.str().c_str(), 64);
 		text2.addString(buffer, 15, 15, 1, STRING_DRAW_ABSOLUTE_BL);
 
+		oss2.str("");
+		oss2.clear();
+		oss2 << (int)get_fps() << " FPS";
+		mbstowcs(buffer, oss2.str().c_str(), 64);
+		text_fps.clearStrings();
+		text_fps.addString(buffer, 75, 15, 1, STRING_DRAW_ABSOLUTE_TR);
+
 		if(camera.projChanged()){
 			int w, h;
 			window_handler.getFramebufferSize(w, h);
 			text->onFramebufferSizeUpdate(w, h);
 			text2.onFramebufferSizeUpdate(w, h);
+			text_fps.onFramebufferSizeUpdate(w, h);
 		}
 		text->render();
 		text2.render();
+		text_fps.render();
 		glEnable(GL_DEPTH_TEST);
 		
 		glfwSwapBuffers(window_handler.getWindow());
-		update_fps_counter(window_handler.getWindow());
 	}
 
 	log("Terminating GLFW and exiting");
