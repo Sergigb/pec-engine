9199416d6c3ca18fb5afc2b62b6d8dd8195656fa
Sergi Garcia <sergi.garciab@e-campus.uab.cat>
2020-03-22 00:32:55 +0100

    testing the new classes and other stuff

--- src/opengl.cpp
+++ src/opengl.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <sstream>
 #include <algorithm>
+#include <vector>
 
 #include "log.hpp"
 #include "utils.hpp"
@@ -20,6 +21,7 @@
 #include "Model.hpp"
 #include "BtWrapper.hpp"
 #include "Object.hpp"
+#include "RenderContext.hpp"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
@@ -31,9 +33,6 @@
 
 
 int main(){
-	int view_mat_location, proj_mat_location, light_pos_location;
-	math::vec3 inital_position = math::vec3(-0.0f, 50.0f, 50.0f);
-	GLFWwindow *g_window = nullptr;
 	int gl_width = 640, gl_height = 480;
 
 	if(change_cwd_to_selfpath() == EXIT_FAILURE)
@@ -41,37 +40,19 @@ int main(){
 
 	log_start();
 
-	if(init_gl(&g_window, gl_width, gl_height) == EXIT_FAILURE)
-		return EXIT_FAILURE;
-	log_gl_params();
-
 	Input input;
-	Camera camera(&inital_position, 67.0f, (float)gl_width / (float)gl_height , 0.1f, 10000000.0f, &input);
-	WindowHandler window_handler(g_window, gl_width, gl_height, &input, &camera);
+	Camera camera(math::vec3(-0.0f, 50.0f, 50.0f), 67.0f, (float)gl_width / (float)gl_height , 0.1f, 100000.0f, &input);
+    camera.setSpeed(10.f);
+	WindowHandler window_handler(gl_width, gl_height, &input, &camera);
+    camera.setWindow(window_handler.getWindow());
 	Frustum frustum;
-	DebugOverlay debug_overlay(&camera, &window_handler);
-	camera.setSpeed(10.f);
-	camera.setWindow(window_handler.getWindow());
-
-	/////////////////////////////////// shader
-
-	GLuint shader_programme = create_programme_from_files("../shaders/phong_blinn_color_vs.glsl",
-														  "../shaders/phong_blinn_color_fs.glsl");
-	log_programme_info(shader_programme);
-	view_mat_location = glGetUniformLocation(shader_programme, "view");
-	proj_mat_location = glGetUniformLocation(shader_programme, "proj");
-	light_pos_location = glGetUniformLocation(shader_programme, "light_position_world");
-
-	glUseProgram(shader_programme);
-	glUniformMatrix4fv(view_mat_location, 1, GL_FALSE, camera.getViewMatrix().m);
-	glUniformMatrix4fv(proj_mat_location, 1, GL_FALSE, camera.getProjMatrix().m);
-	glUniform3fv(light_pos_location, 1, math::vec3(150.0, 100.0, 0.0).v);
+    RenderContext render_context(&camera, &window_handler);
 
 	//////////////////////////////////////
 
-	Model* cube_model = new Model("../data/cube.dae", nullptr, shader_programme, &frustum, math::vec3(0.5, 0.0, 0.5));
-	Model* terrain_model = new Model("../data/bigcube.dae", nullptr, shader_programme, &frustum, math::vec3(0.75, 0.75, 0.75));
-    Model* sphere_model = new Model("../data/sphere.dae", nullptr, shader_programme, &frustum, math::vec3(0.75, 0.75, 0.75));
+	Model* cube_model = new Model("../data/cube.dae", nullptr, render_context.getShader(SHADER_PHONG_BLINN_NO_TEXTURE), &frustum, math::vec3(0.5, 0.0, 0.5));
+	Model* terrain_model = new Model("../data/bigcube.dae", nullptr, render_context.getShader(SHADER_PHONG_BLINN_NO_TEXTURE), &frustum, math::vec3(0.75, 0.75, 0.75));
+    Model* sphere_model = new Model("../data/sphere.dae", nullptr, render_context.getShader(SHADER_PHONG_BLINN_NO_TEXTURE), &frustum, math::vec3(0.75, 0.75, 0.75));
 
     ////////////// bullet test //////////////
 
@@ -108,19 +89,33 @@ int main(){
     Object* sphere2 = new Object(sphere_model, &bt_wrapper, sphere_shape, btVector3(0.0, 55.0, 0.0), btVector3(0.0, 0.0, 0.0), quat, btScalar(10.0));
     sphere2->setColor(math::vec3(1.0, 0.0, 1.0));
 
+    for(int i=0; i<100; i++){
+        Object* cube = new Object(cube_model, &bt_wrapper, cube_shape, btVector3(0.0, 55.0 + (i+1)*5, 0.0), btVector3(0.0, 0.0, 0.0), quat, btScalar(10.0));
+        cube->setColor(math::vec3(1.0, 0.0, 0.0));
+        render_context.m_objects.push_back(cube);
+    }
+
+    for(int i=0; i<100; i++){
+        Object* sphere = new Object(sphere_model, &bt_wrapper, sphere_shape, btVector3(5.0, 55.0 + (i+1)*5, 0.0), btVector3(0.0, 0.0, 0.0), quat, btScalar(10.0));
+        sphere->setColor(math::vec3(0.0, 1.0, 0.0));
+        render_context.m_objects.push_back(sphere);
+    }
+
+    render_context.m_objects.push_back(ground);
+    render_context.m_objects.push_back(cube1);
+    render_context.m_objects.push_back(cube2);
+    render_context.m_objects.push_back(sphere1);
+    render_context.m_objects.push_back(sphere2);
+
     ///////////////////////////////////////
 
-
-	glClearColor(0.2, 0.2, 0.2, 1.0);
+    render_context.setLightPosition(math::vec3(150.0, 100.0, 0.0));
     bool physics_pause = true;
 	while (!glfwWindowShouldClose(window_handler.getWindow())){
 		input.update();
 		camera.update();
 		window_handler.update();
 		frustum.extractPlanes(camera.getViewMatrix(), camera.getProjMatrix(), false);
-
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glClearColor(0.2, 0.2, 0.2, 1.0);
 
         /// bullet simulation step
         // this way we tie the simulation update rate to the framerate, should be 60hz if we limit it to 60 fps. We should manage the physics in a different thread and limit it to 60 hz
@@ -132,33 +127,11 @@ int main(){
             bt_wrapper.stepSimulation(1.f / 60.f, 1);
 
 		// rendering
-        glUseProgram(shader_programme);
-		if(camera.hasMoved())
-			glUniformMatrix4fv(view_mat_location, 1, GL_FALSE, camera.getViewMatrix().m);
-		if(camera.projChanged())
-			glUniformMatrix4fv(proj_mat_location, 1, GL_FALSE, camera.getProjMatrix().m);
-		int num_rendered = 0;
-
-        num_rendered += ground->render();
-        num_rendered += cube1->render();
-        num_rendered += cube2->render();
-        num_rendered += sphere1->render();
-        num_rendered += sphere2->render();
-
-        /////////////////////////////
-
-		debug_overlay.setRenderedObjects(num_rendered);
-		debug_overlay.render();
+        render_context.render();
 
 		glfwSwapBuffers(window_handler.getWindow());
 	}
 
-    delete ground;
-    delete cube1;
-    delete cube2;
-    delete sphere1;
-    delete sphere2;
-    
     delete terrain_model;
     delete cube_model;
     delete sphere_model;
@@ -170,6 +143,12 @@ int main(){
         collisionShapes[j] = 0;
         delete shape;
     }
+
+    for(uint i=0; i<render_context.m_objects.size(); i++){
+        std::cout << "deleted element at " << i << std::endl;
+        delete render_context.m_objects.at(i);
+    }
+    render_context.m_objects.clear();
 
     /////////////
 
