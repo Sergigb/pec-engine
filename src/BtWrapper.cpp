f60a3e0a9012db98fee711947d5618278a43b66a
Sergi Garcia <sergi.garciab@e-campus.uab.cat>
2020-04-02 22:49:33 +0200

    deleted mouse pick function and added testRay function

--- src/BtWrapper.cpp
+++ src/BtWrapper.cpp
@@ -53,39 +53,13 @@ void BtWrapper::deleteBody(btRigidBody* body){
 }
 
 
-Object* BtWrapper::testMousePick(float fb_width, float fb_height, float mouse_x, float mouse_y, const math::mat4& proj_matrix, const math::mat4& view_matrix, double dist){
-    math::vec4 ray_start, ray_start_world;
-    math::vec4 ray_end, ray_end_world;
-    math::mat4 M;
-    math::vec3 ray_dir, ray_end_world_ext;
-
-    // the extraction of the ray could be a method in the camera class at some point, if we need it
-
-    ray_start = math::vec4((mouse_x/fb_width - 0.5) * 2.0,
-                           (mouse_y/fb_height - 0.5) * 2.0,
-                           -1.0, 1.0);
-    ray_end = math::vec4((mouse_x/fb_width - 0.5) * 2.0,
-                         (mouse_y/fb_height - 0.5) * 2.0,
-                         0.0, 1.0);
-
-    M = math::inverse(proj_matrix * view_matrix);
-
-    ray_start_world = M * ray_start;
-    ray_start_world = ray_start_world / ray_start_world.v[3];
-    ray_end_world = M * ray_end;
-    ray_end_world = ray_end_world / ray_end_world.v[3];
-
-    ray_dir = math::normalise(ray_end_world - ray_start_world);
-
-    ray_end_world_ext = math::vec3(ray_start_world) + ray_dir * dist; // ray end extended according to dist (in meters)
-
-
+Object* BtWrapper::testRay(const math::vec3& ray_start_world, const math::vec3& ray_end_world) const{
     btCollisionWorld::ClosestRayResultCallback ray_callback(
             btVector3(ray_start_world.v[0], ray_start_world.v[1], ray_start_world.v[2]),
-            btVector3(ray_end_world_ext.v[0], ray_end_world_ext.v[1], ray_end_world_ext.v[2]));
+            btVector3(ray_end_world.v[0], ray_end_world.v[1], ray_end_world.v[2]));
     m_dynamicsWorld->rayTest(
             btVector3(ray_start_world.v[0], ray_start_world.v[1], ray_start_world.v[2]), 
-            btVector3(ray_end_world_ext.v[0], ray_end_world_ext.v[1], ray_end_world_ext.v[2]),
+            btVector3(ray_end_world.v[0], ray_end_world.v[1], ray_end_world.v[2]),
             ray_callback);
 
     if(ray_callback.hasHit()) {
--- src/BtWrapper.hpp
+++ src/BtWrapper.hpp
@@ -28,7 +28,7 @@ class BtWrapper{
         void addRigidBody(btRigidBody* body);
         void addConstraint(btTypedConstraint *constraint, bool disable_collision_between_bodies);
         void deleteBody(btRigidBody* body);
-        Object* testMousePick(float fb_width, float fb_height, float mouse_x, float mouse_y, const math::mat4& proj_matrix, const math::mat4& view_matrix, double dist);
+        Object* testRay(const math::vec3& ray_start_world, const math::vec3& ray_end_world) const;
 
         void stepSimulation(btScalar time_step, int max_sub_steps);
         
