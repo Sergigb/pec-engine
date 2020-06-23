2f49c16400185a880ef5bd05cf7c1b7f846bc4ca
Sergi Garcia <sergi.garciab@e-campus.uab.cat>
2020-06-24 01:05:21 +0200

    added siple gui shaders

---
+++ shaders/gui_fs.glsl
@@ -0,0 +1,10 @@
+#version 410
+
+out vec4 frag_colour;
+
+// simple panel drawing
+
+void main(){
+    frag_colour = vec4(0.0, 0.8, 0.8, 1.0);
+}
+
---
+++ shaders/gui_vs.glsl
@@ -0,0 +1,10 @@
+#version 410
+layout(location = 0) in vec2 vertex;
+
+uniform mat4 projection;
+
+// simple panel drawing
+
+void main(){
+    gl_Position = projection * vec4(vertex, 0.0, 1.0); //should work
+}
