11dda947f2d7f3b0b31a8780fde6d05eead9665a
Sergi Garcia <sergi.garciab@e-campus.uab.cat>
2020-07-14 22:00:00 +0200

    added some interactivity with the buttons and other minor modifications

--- src/EditorGUI.cpp
+++ src/EditorGUI.cpp
@@ -8,12 +8,24 @@ EditorGUI::EditorGUI(){
 }
 
 
-EditorGUI::EditorGUI(const WindowHandler* window_handler, FontAtlas* atlas, GLuint shader, const RenderContext* render_context): BaseGUI(window_handler){
+EditorGUI::EditorGUI(const WindowHandler* window_handler, FontAtlas* atlas, GLuint shader, const RenderContext* render_context, const Input* input): BaseGUI(window_handler){
+    int fb_height, fb_width;
+
     m_fb_update = true;
     m_init = true;
     m_font_atlas = atlas;
     m_shader_programme = shader;
     m_render_context = render_context;
+    m_input = input;
+    m_button_mouseover = -1;
+    m_last_button_color = -1;
+    m_button_select = -1;
+    std::memset(m_button_status, 0, EDITOR_GUI_N_BUTTONS * sizeof(bool));
+    std::memset(m_button_color_status, 0, EDITOR_GUI_N_BUTTONS * sizeof(bool));
+
+    m_window_handler->getFramebufferSize(fb_width, fb_height);
+    m_fb_width_f = (float)fb_width;
+    m_fb_height_f = (float)fb_height;
 
     // gl init
     glGenVertexArrays(1, &m_vao);
@@ -29,25 +41,25 @@ EditorGUI::EditorGUI(const WindowHandler* window_handler, FontAtlas* atlas, GLui
     glVertexAttribPointer(1, 4,  GL_FLOAT, GL_FALSE, 0, NULL);
     glEnableVertexAttribArray(1);
     // color never changes so there's no reason to change it in the updateBuffers method
-    float gui_color[4 * EDITOR_GUI_VERTEX_NUM] = {0.4, 0.4, 0.4, 1.0,
-                                                  0.4, 0.4, 0.4, 1.0,
-                                                  0.4, 0.4, 0.4, 1.0,
-                                                  0.4, 0.4, 0.4, 1.0,
-                                                  0.4, 0.4, 0.4, 1.0,
-                                                  0.4, 0.4, 0.4, 1.0,
-                                                  0.4, 0.4, 0.4, 1.0,
-                                                  0.25, 0.25, 0.25, 1.0,
-                                                  0.25, 0.25, 0.25, 1.0,
-                                                  0.25, 0.25, 0.25, 1.0,
-                                                  0.25, 0.25, 0.25, 1.0,
-                                                  0.25, 0.25, 0.25, 1.0,
-                                                  0.25, 0.25, 0.25, 1.0,
-                                                  0.25, 0.25, 0.25, 1.0,
-                                                  0.25, 0.25, 0.25, 1.0,
-                                                  0.25, 0.25, 0.25, 1.0,
-                                                  0.25, 0.25, 0.25, 1.0,
-                                                  0.25, 0.25, 0.25, 1.0,
-                                                  0.25, 0.25, 0.25, 1.0};
+    GLfloat gui_color[4 * EDITOR_GUI_VERTEX_NUM] = {EDITOR_GUI_PANEL_COLOR,
+                                                    EDITOR_GUI_PANEL_COLOR,
+                                                    EDITOR_GUI_PANEL_COLOR,
+                                                    EDITOR_GUI_PANEL_COLOR,
+                                                    EDITOR_GUI_PANEL_COLOR,
+                                                    EDITOR_GUI_PANEL_COLOR,
+                                                    EDITOR_GUI_PANEL_COLOR,
+                                                    BUTTON_COLOR_DEFAULT,
+                                                    BUTTON_COLOR_DEFAULT,
+                                                    BUTTON_COLOR_DEFAULT,
+                                                    BUTTON_COLOR_DEFAULT,
+                                                    BUTTON_COLOR_DEFAULT,
+                                                    BUTTON_COLOR_DEFAULT,
+                                                    BUTTON_COLOR_DEFAULT,
+                                                    BUTTON_COLOR_DEFAULT,
+                                                    BUTTON_COLOR_DEFAULT,
+                                                    BUTTON_COLOR_DEFAULT,
+                                                    BUTTON_COLOR_DEFAULT,
+                                                    BUTTON_COLOR_DEFAULT};
 
     glBufferData(GL_ARRAY_BUFFER, 4 * EDITOR_GUI_VERTEX_NUM * sizeof(GLfloat), gui_color, GL_STATIC_DRAW);
 
@@ -76,7 +88,8 @@ EditorGUI::EditorGUI(const WindowHandler* window_handler, FontAtlas* atlas, GLui
     index_buffer[10] = 5;
     index_buffer[11] = 6;
 
-    for(char i=0; i < 3; i++){
+    // buttons if I'm not mistaken
+    for(char i=0; i < EDITOR_GUI_N_BUTTONS; i++){
         int disp = i * 4;
 
         index_buffer[12 + i * 6] = disp + 7;
@@ -107,42 +120,43 @@ void EditorGUI::onFramebufferSizeUpdate(){
 
 void EditorGUI::updateBuffers(){
     int fb_height, fb_width;
-    float fb_height_f, fb_width_f, x_start;
+    float x_start;
 
     GLfloat vertex_buffer[2 * EDITOR_GUI_VERTEX_NUM];
 
     m_window_handler->getFramebufferSize(fb_width, fb_height);
-    fb_width_f = (float)fb_width;
-    fb_height_f= (float)fb_height;
+    m_fb_width_f = (float)fb_width;
+    m_fb_height_f = (float)fb_height;
 
-    // base panels
+    // left panel
     vertex_buffer[0] = 0.0;
     vertex_buffer[1] = 0.0;
     vertex_buffer[2] = 0.0;
-    vertex_buffer[3] = fb_height_f;
+    vertex_buffer[3] = m_fb_height_f;
     vertex_buffer[4] = EDITOR_GUI_LP_W;
     vertex_buffer[5] = 0.0;
     vertex_buffer[6] = EDITOR_GUI_LP_W;
-    vertex_buffer[7] = fb_height_f;
+    // top panel
+    vertex_buffer[7] = m_fb_height_f;
     vertex_buffer[8] = 0.0;
-    vertex_buffer[9] = fb_height_f - EDITOR_GUI_TP_H;
-    vertex_buffer[10] = fb_width_f;
-    vertex_buffer[11] = fb_height_f - EDITOR_GUI_TP_H;
-    vertex_buffer[12] = fb_width_f;
-    vertex_buffer[13] = fb_height_f;
-    // buttons
+    vertex_buffer[9] = m_fb_height_f - EDITOR_GUI_TP_H;
+    vertex_buffer[10] = m_fb_width_f;
+    vertex_buffer[11] = m_fb_height_f - EDITOR_GUI_TP_H;
+    vertex_buffer[12] = m_fb_width_f;
+    vertex_buffer[13] = m_fb_height_f;
 
-    for(char i=0; i < 3; i++){
+    // buttons
+    for(char i=0; i < EDITOR_GUI_N_BUTTONS; i++){
         x_start = (i+1) * BUTTON_PAD_X + i * BUTTON_SIZE_X;
 
         vertex_buffer[14 + i * 8] = x_start; //1
-        vertex_buffer[15 + i * 8] = fb_height_f - BUTTON_PAD_Y;
+        vertex_buffer[15 + i * 8] = m_fb_height_f - BUTTON_PAD_Y;
         vertex_buffer[16 + i * 8] = x_start;
-        vertex_buffer[17 + i * 8] = fb_height_f - BUTTON_PAD_Y - BUTTON_SIZE_Y;
+        vertex_buffer[17 + i * 8] = m_fb_height_f - BUTTON_PAD_Y - BUTTON_SIZE_Y;
         vertex_buffer[18 + i * 8] = x_start + BUTTON_SIZE_X;
-        vertex_buffer[19 + i * 8] = fb_height_f - BUTTON_PAD_Y - BUTTON_SIZE_Y;
+        vertex_buffer[19 + i * 8] = m_fb_height_f - BUTTON_PAD_Y - BUTTON_SIZE_Y;
         vertex_buffer[20 + i * 8] = x_start + BUTTON_SIZE_X;
-        vertex_buffer[21 + i * 8] = fb_height_f - BUTTON_PAD_Y;
+        vertex_buffer[21 + i * 8] = m_fb_height_f - BUTTON_PAD_Y;
     }
 
     m_render_context->bindVao(m_vao);
@@ -154,12 +168,72 @@ void EditorGUI::updateBuffers(){
     glBufferData(GL_ARRAY_BUFFER, 2 * m_num_vertices * sizeof(GLfloat), tex_coords_buffer.get(), GL_STATIC_DRAW);*/
 }
 
+void EditorGUI::colorButton(const GLfloat* color_array, int button){
+        m_render_context->bindVao(m_vao);
+        glBindBuffer(GL_ARRAY_BUFFER, m_vbo_clr);
+
+        glBufferSubData(GL_ARRAY_BUFFER, ((7*4) + (button * 16)) * sizeof(GLfloat) , 16 * sizeof(GLfloat), color_array);
+}
+
+
+void EditorGUI::updateButtons(){ // used to update button colors
+    // grab the current value of m_button_mouseover in case it is overwritten in the update function while we are working with it
+    int button_mouseover = m_button_mouseover;
+
+    // reset button color if m_last_button_color != -1
+    if(m_last_button_color >= 0){
+        GLfloat new_color[16] = {BUTTON_COLOR_DEFAULT,
+                                 BUTTON_COLOR_DEFAULT,
+                                 BUTTON_COLOR_DEFAULT,
+                                 BUTTON_COLOR_DEFAULT};
+
+        colorButton(new_color, m_last_button_color);
+    }
+    m_last_button_color = -1;
+
+    if(button_mouseover >= 0 && button_mouseover < EDITOR_GUI_N_BUTTONS){ // && button_mouseover < EDITOR_GUI_N_BUTTONS -> sanity check
+        if(!m_button_color_status[button_mouseover]){
+            GLfloat new_color[16] = {BUTTON_COLOR_MOUSEOVER,
+                                     BUTTON_COLOR_MOUSEOVER,
+                                     BUTTON_COLOR_MOUSEOVER,
+                                     BUTTON_COLOR_MOUSEOVER};
+
+            colorButton(new_color, button_mouseover);
+
+            m_last_button_color = button_mouseover;
+        }
+    }
+
+    for(int i=0; i < EDITOR_GUI_N_BUTTONS; i++){
+        if(m_button_color_status[i] != m_button_status[i]){
+            m_button_color_status[i] = m_button_status[i];
+
+            if(m_button_color_status[i]){
+                GLfloat new_color[16] = {BUTTON_COLOR_SELECTED,
+                                         BUTTON_COLOR_SELECTED,
+                                         BUTTON_COLOR_SELECTED,
+                                         BUTTON_COLOR_SELECTED};
+
+                colorButton(new_color, i);
+            }
+            else{
+                GLfloat new_color[16] = {BUTTON_COLOR_DEFAULT,
+                                         BUTTON_COLOR_DEFAULT,
+                                         BUTTON_COLOR_DEFAULT,
+                                         BUTTON_COLOR_DEFAULT};
+                colorButton(new_color, i);
+            }
+        }
+    }
+}
+
 
 void EditorGUI::render(){
     if(m_fb_update){
         updateBuffers();
         m_fb_update = false;
     }
+    updateButtons();
 
     m_render_context->useProgram(m_shader_programme);
     m_render_context->bindVao(m_vao);
@@ -170,6 +244,28 @@ void EditorGUI::render(){
 
 
 void EditorGUI::update(){
-    // deal with input??
+    double posx, posy;
+
+    m_input->getMousePos(posx, posy);
+    posx = (double)posx;
+    posy = m_fb_height_f - (double)posy;
+
+    m_button_mouseover = -1;
+    if(posy > m_fb_height_f - EDITOR_GUI_TP_H){ // mouse over top panel
+        for(int i=0; i < EDITOR_GUI_N_BUTTONS; i++){
+            float x_start = (i+1) * BUTTON_PAD_X + i * BUTTON_SIZE_X;
+            
+            if(posx > x_start && posx < x_start + BUTTON_SIZE_X && posy > m_fb_height_f - 
+               BUTTON_PAD_Y - BUTTON_SIZE_Y && posy < m_fb_height_f - BUTTON_PAD_Y){
+
+                if(m_input->pressed_mbuttons[GLFW_MOUSE_BUTTON_1] & INPUT_MBUTTON_PRESS){
+                    m_button_status[i] = !m_button_status[i];
+                }
+                else{
+                    m_button_mouseover = i;
+                }
+            }
+        }
+    }
 }
 
--- src/EditorGUI.hpp
+++ src/EditorGUI.hpp
@@ -9,22 +9,39 @@
 #include "common.hpp"
 #include "FontAtlas.hpp"
 #include "RenderContext.hpp"
+#include "Input.hpp"
 
 // not sure if it's a good idea to hardcode this
 #define EDITOR_GUI_VERTEX_NUM 19
 #define EDITOR_GUI_INDEX_NUM 30
 #define EDITOR_GUI_LP_W 300.0
 #define EDITOR_GUI_TP_H 40.0
+#define EDITOR_GUI_N_BUTTONS 3 // changing this won't add buttons cuz the array sizes are fixed
+
+// colors are defined per vertex
+#define EDITOR_GUI_PANEL_COLOR 0.15, 0.15, 0.15, 1.0
 
 #define BUTTON_PAD_X 20
 #define BUTTON_PAD_Y 5
 #define BUTTON_SIZE_X 100
 #define BUTTON_SIZE_Y 30
 
+#define BUTTON_COLOR_DEFAULT 0.25, 0.25, 0.25, 1.0
+#define BUTTON_COLOR_MOUSEOVER 0.35, 0.35, 0.35, 1.0
+#define BUTTON_COLOR_SELECTED 0.1, 0.0, 1., 1.0
+
+
 
 class EditorGUI : public BaseGUI{
     private:
         bool m_fb_update, m_init;
+        float m_fb_height_f, m_fb_width_f;
+        int m_button_mouseover, m_button_select;
+        bool m_button_status[EDITOR_GUI_N_BUTTONS];
+
+        // used by the render thread
+        int m_last_button_color; // last button that changed color
+        bool m_button_color_status[EDITOR_GUI_N_BUTTONS];
 
         GLuint m_vao, m_vbo_vert, m_vbo_tex, m_vbo_ind, m_vbo_clr;
         GLuint m_shader_programme;
@@ -33,11 +50,14 @@ class EditorGUI : public BaseGUI{
 
         FontAtlas* m_font_atlas;
         const RenderContext* m_render_context;
+        const Input* m_input;
 
         void updateBuffers();
+        void updateButtons();
+        void colorButton(const GLfloat* color_array, int button);
     public:
         EditorGUI();
-        EditorGUI(const WindowHandler* window_handler, FontAtlas* atlas, GLuint shader, const RenderContext* render_context);
+        EditorGUI(const WindowHandler* window_handler, FontAtlas* atlas, GLuint shader, const RenderContext* render_context, const Input* input);
         ~EditorGUI();
 
         void onFramebufferSizeUpdate();
