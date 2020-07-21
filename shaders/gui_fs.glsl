096cabbe495e05145104696ea3a6f5749b081ee9
Sergi Garcia <sergi.garciab@e-campus.uab.cat>
2020-07-22 01:40:38 +0200

    added texture support in GUI shader

--- shaders/gui_fs.glsl
+++ shaders/gui_fs.glsl
@@ -1,11 +1,16 @@
 #version 410
 
 in vec4 vert_color;
+in vec3 sta;
+
 out vec4 frag_colour;
+
+uniform sampler2D texture_sampler;
 
 // simple panel drawing
 
 void main(){
-    frag_colour = vert_color;
+    vec4 sampled = texture(texture_sampler, sta.xy) * sta.z;
+    frag_colour = vert_color + sampled;
 }
 
--- shaders/gui_vs.glsl
+++ shaders/gui_vs.glsl
@@ -1,14 +1,17 @@
 #version 410
 layout(location = 0) in vec2 vertex;
 layout(location = 1) in vec4 color;
+layout(location = 2) in vec3 tex_coord_alpha;
 
 uniform mat4 projection;
 
 out vec4 vert_color;
+out vec3 sta;
 
 // simple panel drawing
 
 void main(){
     vert_color = color;
+    sta = tex_coord_alpha;
     gl_Position = projection * vec4(vertex, 0.0, 1.0); //should work
 }
