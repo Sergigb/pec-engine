6c72836fa0697f9e5ffc67103903051fd0e69c4a
Sergi Garcia <sergi.garciab@e-campus.uab.cat>
2020-08-06 00:06:20 +0200

    forgot to commit this, new return type in the update method

--- src/BaseGUI.hpp
+++ src/BaseGUI.hpp
@@ -16,7 +16,7 @@ class BaseGUI{
 
         virtual void onFramebufferSizeUpdate() = 0;
         virtual void render() = 0;
-        virtual void update() = 0;
+        virtual int update() = 0;
 };
 
 
