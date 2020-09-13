4e60d52b009c59eeead3868806bd1bfe041c9d42
Sergigb <sergi.garciab@e-campus.uab.cat>
2020-09-14 00:28:16 +0200

    added planet radious

--- shaders/planet_vs.glsl
+++ shaders/planet_vs.glsl
@@ -4,11 +4,12 @@ layout(location = 0) in vec3 vertex_position;
 layout(location = 1) in vec3 vertex_normal;
 
 uniform mat4 view, proj, model, relative_planet;
+uniform float planet_radius;
 
 out vec3 normal_eye, position_eye;
 
 void main() {
-	vec3 position_eye_norm = normalize(vec3(relative_planet * vec4(vertex_position, 1.0)));
+	vec3 position_eye_norm = normalize(vec3(relative_planet * vec4(vertex_position, 1.0))) * planet_radius;
 	position_eye = vec3(view * model * vec4(position_eye_norm, 1.0));
     normal_eye = vec3(view * model * vec4(position_eye_norm, 0.0));
     gl_Position = proj * vec4 (position_eye, 1.0);
