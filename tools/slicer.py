812d5a1c703842419507a5f741991b05ccdeca4f
Sergigb <sergi.garciab@e-campus.uab.cat>
2020-09-20 01:49:33 +0200

    fixed range

--- tools/slicer.py
+++ tools/slicer.py
@@ -6,7 +6,7 @@ top_level_images = ["0_0_0.png", "1_0_0.png", "2_0_0.png", "3_0_0.png", "4_0_0.p
 
 destination_folder = "../data/earth_textures/"
 
-level = 2
+level = 3
 
 for top_level_image in top_level_images:
     side = int(top_level_image.split("_")[0])
@@ -15,12 +15,14 @@ for top_level_image in top_level_images:
     image = np.array(Image.open(path + top_level_image))
     size = image.shape[0]
     subdivision_size = size / n
+    print(subdivision_size)
 
     for i in range(n):
         for j in range(n):
-            patch = image[(subdivision_size / n) * i : (subdivision_size / n) * (i + 1),
-                          (subdivision_size / n) * j : (subdivision_size / n) * (j + 1), :]
+            patch = image[(subdivision_size) * i : (subdivision_size) * (i + 1),
+                          (subdivision_size) * j : (subdivision_size) * (j + 1), :]
             filename = str(level) + "_" + str(side) + "_" + str(i) + "_" + str(j) + ".png"
+            print(patch.shape)
 
             patch = Image.fromarray(patch)
             patch.save(destination_folder + filename)
