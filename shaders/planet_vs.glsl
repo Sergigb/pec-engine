6db0eacd31432939dc20f73f228d8c5d02e6a0dc
Sergigb <sergi.garciab@e-campus.uab.cat>
2020-09-27 00:48:01 +0200

    changed named of the texture scale uniform

--- shaders/planet_vs.glsl
+++ shaders/planet_vs.glsl
@@ -5,14 +5,14 @@ layout(location = 1) in vec3 vertex_normal;
 
 uniform mat4 view, proj, model, relative_planet;
 uniform vec2 tex_shift;
-uniform float planet_radius, patch_scale;
+uniform float planet_radius, texture_scale;
 uniform sampler2D elevation;
 
 out vec3 normal_eye, position_eye;
 out vec2 st;
 
 void main() {
-    st = ((-vertex_position.zy) * patch_scale) - (tex_shift.yx - 0.5);
+    st = ((-vertex_position.zy) * texture_scale) - (tex_shift.yx - 0.5);
     float height = texture(elevation, st).r * 6400; // 6400 max elevation heightmap, change to uniform
 
     vec3 position_eye_norm = normalize(vec3(relative_planet * vec4(vertex_position, 1.0))) * (planet_radius + height);
