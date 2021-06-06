e9500dd64703435bee55fededc8002537a6d0fbb
Sergigb <sergi.garciab@e-campus.uab.cat>
2021-06-06 20:50:34 +0200

    i dont understand git

---
+++ src/PlanetRenderer.hpp
@@ -0,0 +1,42 @@
+#ifndef PLANET_RENDERER_HPP
+#define PLANET_RENDERER_HPP
+
+#include <GL/glew.h>
+#include <memory>
+#include <vector>
+
+#include "core/BaseApp.hpp"
+
+
+#define SIDE_PX 0
+#define SIDE_NX 1
+#define SIDE_PY 2
+#define SIDE_NY 3
+#define SIDE_PZ 4
+#define SIDE_NZ 5
+
+#define TEXTURE_LOCATION 0
+#define ELEVATION_LOCATION 1
+
+
+class FontAtlas;
+
+
+class PlanetRenderer : public BaseApp{
+    private:
+        void init();
+
+        // application default font atlas
+        std::unique_ptr<FontAtlas> m_def_font_atlas;
+
+        void render_side(struct surface_node& surface_subtree, math::mat4& planet_transform, int level, dmath::vec3& cam_origin, double sea_level);
+    public:
+        PlanetRenderer();
+        PlanetRenderer(int gl_width, int gl_height);
+        ~PlanetRenderer();
+
+        void run();
+};
+
+
+#endif
---
+++ src/main_planet_renderer.cpp
@@ -0,0 +1,29 @@
+#include "PlanetRenderer.hpp"
+#include "core/log.hpp"
+#include "core/utils/utils.hpp"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image_write.h>
+#include <stb/stb_image.h>
+
+
+int main(int argc, char* argv[]){
+    UNUSED(argc); UNUSED(argv);
+
+    if(change_cwd_to_selfpath() == EXIT_FAILURE)
+        std::cerr << "Could not change the cwd to executable path, proceeding" << std::endl;
+
+    log_start();
+
+    PlanetRenderer* p = new PlanetRenderer();
+    p->run();
+    delete p;
+
+            while(1){
+        
+    }
+
+    return EXIT_SUCCESS;
+}
+
