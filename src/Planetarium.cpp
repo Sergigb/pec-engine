763fd81d92110413abc3c9f2d805c6432605df49
Sergigb <sergi.garciab@e-campus.uab.cat>
2021-06-07 01:24:56 +0200

    primitive rendering of the planetarium

--- src/Planetarium.cpp
+++ src/Planetarium.cpp
@@ -1,14 +1,21 @@
 #include <unordered_map>
 #include <ctime>
 #include <cmath>
+#include <cstdlib>
+#include <iomanip>
 
 #include "Planetarium.hpp"
 #include "core/log.hpp"
 #include "core/utils/utils.hpp"
-#include "core/loading/load_star_system.hpp"
 #include "core/maths_funcs.hpp"
 #include "core/RenderContext.hpp"
+#include "core/Camera.hpp"
+#include "core/Input.hpp"
+#include "core/WindowHandler.hpp"
+#include "core/Player.hpp"
+#include "core/Frustum.hpp"
 #include "GUI/FontAtlas.hpp"
+#include "GUI/Text2D.hpp"
 
 
 Planetarium::Planetarium() : BaseApp(){
@@ -22,6 +29,9 @@ Planetarium::Planetarium(int gl_width, int gl_height) : BaseApp(gl_width, gl_hei
 
 
 void Planetarium::init(){
+    m_delta_t = (10000000000. / 60.0) / 1000.0;  // in ms
+    load_star_system(m_system);
+
     m_render_context->setLightPosition(math::vec3(150.0, 100.0, 0.0));
 
     m_def_font_atlas.reset(new FontAtlas(256));
@@ -29,6 +39,14 @@ void Planetarium::init(){
     m_def_font_atlas->loadCharacterRange(32, 255); // ascii
     m_def_font_atlas->loadCharacterRange(913, 1023); // greek and coptic
     m_def_font_atlas->createAtlas(false);
+
+    int fb_width, fb_height;
+    m_window_handler->getFramebufferSize(fb_width, fb_height);
+
+    m_text.reset(new Text2D(fb_width, fb_height, {0.f, 1.f, 1.f}, 
+                            m_def_font_atlas.get(), m_render_context.get()));
+    m_text2.reset(new Text2D(fb_width, fb_height, {0.f, 1.f, 0.f}, 
+                            m_def_font_atlas.get(), m_render_context.get()));
 }
 
 
@@ -82,11 +100,11 @@ void update_orbital_elements(planetary_system& system, const double centuries_si
                            std::sin(current.long_asc_node) * std::sin(current.arg_periapsis + true_annomaly) *
                            std::cos(current.inclination));
 
-        current.pos.v[1] = rad * (std::sin(current.long_asc_node) * std::cos(current.arg_periapsis + true_annomaly) +
+        current.pos.v[1] = rad * (std::sin(current.inclination) * std::sin(current.arg_periapsis + true_annomaly));
+
+        current.pos.v[2] = rad * (std::sin(current.long_asc_node) * std::cos(current.arg_periapsis + true_annomaly) +
                            std::cos(current.long_asc_node) * std::sin(current.arg_periapsis + true_annomaly) *
                            std::cos(current.inclination));
-
-        current.pos.v[2] = rad * (std::sin(current.inclination) * std::sin(current.arg_periapsis + true_annomaly));
     }
 }
 
@@ -118,15 +136,96 @@ current.pos.v[1] = std::sin(current.long_asc_node) * xtemp +
 */
 
 
+void Planetarium::processInput(){
+    double scx, scy;
+
+    m_input->getScroll(scx, scy);
+
+    if(scy < 0.0)
+        m_delta_t *= 0.1f;
+    if(scy > 0.0)
+        m_delta_t *= 10.f;
+
+    if(m_input->pressed_keys[GLFW_KEY_R] & INPUT_KEY_RELEASE){
+        m_render_context->reloadShaders();
+        std::cout << "Shaders reloaded" << std::endl;
+    }
+
+    if(m_input->pressed_keys[GLFW_KEY_ESCAPE] & INPUT_KEY_RELEASE){
+        glfwSetWindowShouldClose(m_window_handler->getWindow(), GLFW_TRUE);
+    }
+
+    if(m_input->pressed_keys[GLFW_KEY_F12] & INPUT_KEY_RELEASE){
+        m_render_context->toggleDebugOverlay();
+    }
+}
+
+
+void Planetarium::logic(){
+    double centuries_since_j2000 = 0.0;
+    time_t current_time;
+    wchar_t buff[256];
+    std::ostringstream oss;
+
+    centuries_since_j2000 = m_seconds_since_j2000 / SECONDS_IN_A_CENTURY;
+    update_orbital_elements(m_system, centuries_since_j2000);
+    updateSceneText();
+
+    m_text2->clearStrings();
+
+    current_time = (time_t)m_seconds_since_j2000 + SECS_FROM_UNIX_TO_J2000;
+    mbstowcs(buff, ctime(&current_time), 256);
+    m_text2->addString(buff, 25, 50, 1.0f,
+                      STRING_DRAW_ABSOLUTE_BL, STRING_ALIGN_RIGHT);
+
+    oss << "delta_t: " << m_delta_t << "ms (" << std::setprecision(3) <<
+            m_delta_t / 36000.0 << " hours)";
+    mbstowcs(buff, oss.str().c_str(), 256);
+    m_text2->addString(buff, 25, 35, 1.0f,
+                      STRING_DRAW_ABSOLUTE_BL, STRING_ALIGN_RIGHT);
+
+    m_text->render();
+    m_text2->render();
+
+    m_seconds_since_j2000 += m_delta_t;
+}
+
+
+void Planetarium::updateSceneText(){
+    planet_map::iterator it;
+    planet_map& planets = m_system.planets;
+    const math::mat4 proj_mat = m_camera->getProjMatrix();
+    const math::mat4 view_mat = m_camera->getViewMatrix();
+    int w, h;
+
+    m_window_handler->getFramebufferSize(w, h);
+
+    m_text->clearStrings();
+
+    for(it=planets.begin();it!=planets.end();it++){
+        planet& current = it->second;
+        wchar_t name[32];
+
+        math::vec4 pos(current.pos.v[0] / 1e10, current.pos.v[1] / 1e10, current.pos.v[2] / 1e10, 1.0f);
+        math::vec4 pos_screen = proj_mat * view_mat * pos;
+        pos_screen = ((pos_screen / pos_screen.v[3]) + 1. ) / 2.;
+
+        mbstowcs(name, current.name.c_str(), 32);
+        m_text->addString(name, pos_screen.v[0] * w, pos_screen.v[1] * h, 1.0f,
+                          STRING_DRAW_ABSOLUTE_BL, STRING_ALIGN_CENTER_XY);
+
+    }
+}
+
+
 void Planetarium::run(){
-    /*m_camera->setCameraPosition(dmath::vec3(9300000.0, 0.0, 0.0));
-    m_camera->setSpeed(630000.0f);
-    m_camera->createProjMat(1.0, 63000000, 67.0, 1.0);
+    m_camera->setCameraPosition(dmath::vec3(500.0, 1000.0, 0.0));
+    m_camera->setSpeed(50.0f);
+    m_camera->createProjMat(1.0, 100000.0, 67.0, 1.0);
 
     //glDisable(GL_CULL_FACE);
 
     m_render_context->setLightPosition(math::vec3(63000000000.0, 0.0, 0.0));
-    m_render_context->toggleDebugOverlay();
 
     while(!glfwWindowShouldClose(m_window_handler->getWindow())){
         m_input->update();
@@ -134,43 +233,16 @@ void Planetarium::run(){
         m_frustum->extractPlanes(m_camera->getCenteredViewMatrix(), m_camera->getProjMatrix(), false);
         m_player->update();
 
-        //m_render_context->contextUpdatePlanetarium();
+        m_render_context->contextUpdatePlanetRenderer(); // works in here too :)
+        glClearColor(0.f, 0.f, 0.f, 0.f); // loook at the todo list
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        if(m_input->pressed_keys[GLFW_KEY_R] & INPUT_KEY_RELEASE){
-            m_render_context->reloadShaders();
-            std::cout << "Shaders reloaded" << std::endl;
-        }
+        processInput();
+        logic();
 
         glfwSwapBuffers(m_window_handler->getWindow());
     }
 
-    m_window_handler->terminate();*/
-    double seconds_since_j2000 = 0.0, centuries_since_j2000 = 0.0;
-    double delta_t = (1000. / 60.) / 1000.0;
-    time_t current_time;
-    const char* ctime_now;
-
-    struct planetary_system system;
-    load_star_system(system);
-
-    while(1){
-        centuries_since_j2000 = seconds_since_j2000 / SECONDS_IN_A_CENTURY;
-        update_orbital_elements(system, centuries_since_j2000);
-
-        current_time = (time_t)seconds_since_j2000 + SECS_FROM_UNIX_TO_J2000;
-        ctime_now = ctime(&current_time);
-        std::cout << ctime_now;
-
-        std::hash<std::string> str_hash;
-        planet& earth = system.planets.at(str_hash("Earth"));
-        //dmath::vec3 disp = earth.pos - earth.pos_prev;
-        std::cout << earth.pos.v[0] << ", " << earth.pos.v[1] << ", " << earth.pos.v[2] << std::endl;
-        //double velocity = dmath::length(disp) / delta_t;
-
-        //std::cout.precision(10);
-        //std::cout << "vel earth: " << velocity << std::endl;
-
-        seconds_since_j2000 += delta_t;
-    }
+    m_window_handler->terminate();
 }
 
--- src/Planetarium.hpp
+++ src/Planetarium.hpp
@@ -6,6 +6,7 @@
 #include <vector>
 
 #include "core/BaseApp.hpp"
+#include "core/loading/load_star_system.hpp" // planetary_system defined here, move later
 
 
 #define SECS_FROM_UNIX_TO_J2000 946684800.0
@@ -14,16 +15,23 @@
 
 
 class FontAtlas;
+class Text2D;
 
 
 class Planetarium : public BaseApp{
     private:
+        double m_delta_t, m_seconds_since_j2000;
+        struct planetary_system m_system;
+        std::unique_ptr<Text2D> m_text, m_text2;
+
         void init();
+        void logic();
+        void updateSceneText();
 
         // application default font atlas
         std::unique_ptr<FontAtlas> m_def_font_atlas;
 
-
+        void processInput();
     public:
         Planetarium();
         Planetarium(int gl_width, int gl_height);
