74077f9a40eb8170fde279286b1aa095b9df3509
Sergi Garcia <sergi.garciab@e-campus.uab.cat>
2021-11-09 19:57:28 +0100

    moar documentation

--- src/assets/Resource.hpp
+++ src/assets/Resource.hpp
@@ -18,6 +18,9 @@
 #define RESOURCE_STATE_GAS 3
 
 
+/*
+ * Resource class.
+ */
 class Resource{
     private:
         std::string m_resource_name;
@@ -28,16 +31,60 @@ class Resource{
         double m_temperature; // temperature at which it has to be stored in this state
         std::uint32_t m_resource_id;
     public:
+        /*
+         * Constructor.
+         *
+         * @name: simple name of the resource.
+         * @fancy_name: name of the resource, it can have unicode chars. The encoding of the
+         * @strings sucks right now.
+         * @rtype: type of resource, defined in the macros above (RESOURCE_TYPE_*).
+         * @rstate: physical state of the resource, solid, liquid or gas. Use the macros from
+         * above (RESOURCE_STATE_*).
+         * @density: density of the resource, in kg/M^3.
+         * @temperature: temperature at which it has to be stored in this state.
+         */
         Resource(const std::string& name, const std::string& fancy_name, char rtype, char rstate, double density, double temperature);
+
         ~Resource();
+
+        /*
+         * Sets the ID of the resource.
+         */
         void setId(std::uint32_t id);
 
+        /*
+         * Returns the type of the resource (macros RESOURCE_TYPE_*).
+         */
         char getType() const;
+
+        /*
+         * Returns the state of the resource (macros RESOURCE_STATE_*).
+         */
         char getState() const;
+
+        /*
+         * Returns the density of the resource, in kg/M^3.
+         */
         double getDensity() const;
+
+        /*
+         * Returns the temperature of the resource.
+         */
         double getTemperature() const;
+
+        /*
+         * Returns a constant reference to the name of the resource.
+         */
         const std::string& getName() const;
+
+        /*
+         * Returns a constant reference to the fancy name of the resource.
+         */
         const std::string& getFancyName() const;
+
+        /*
+         * Returns the ID of the resource.
+         */
         std::uint32_t getId() const;
 };
 
--- src/assets/Vessel.hpp
+++ src/assets/Vessel.hpp
@@ -14,6 +14,13 @@ class Input;
 class btVector3;
 
 
+/*
+ * Structs that holds an action of a stage of the vessel. It has a part as a target and
+ * the action.
+ *
+ * @part: pointer to the part.
+ * @action: action that the part should perform.
+ */
 struct stage_action{
     BasePart* part;
     long action;
@@ -24,6 +31,10 @@ struct stage_action{
 };
 
 
+/*
+ * Vessel class, holds the vessel tree and other information. The player should is able to control
+ * vessels.
+ */
 class Vessel{
     private:
         std::string m_vessel_name, m_vessel_desc;
@@ -39,49 +50,210 @@ class Vessel{
         Player* m_player; // player controlling the vessel
         const Input* m_input;
 
-        void updateNodes(); // updates node list and map
+        /*
+         * Updates m_node_list and m_node_map_by_id by traveling through the vessel tree.
+         */
+        void updateNodes();
+
+        /*
+         * Updates the mass of the vessel (m_total_mass). This is not used by bullet buy it may
+         * be used by other physics calculations.
+         */
         void updateMass();
+
+        /*
+         * Crudely updates the staging of the vessel.
+         */
         void updateStaging();
+
+        /*
+         * Recursive method used by updateStaging.
+         */
+
         void updateStagingRec(BasePart* part);
+
+        /*
+         * Triggers the next stage of the vessel.
+         */
         void activateNextStage();
     public:
         Vessel();
+
+        /*
+         * Constructor.
+         *
+         * @vessel_root: root of the vessel, the vessel will take ownership of this pointer.
+         * @input: pointer to the input object.
+         */
         Vessel(std::shared_ptr<BasePart>& vessel_root, const Input* input);
+
         ~Vessel();
 
+        /*
+         * Sets the vessel name.
+         */
         void setVesselName(const std::string& name);
+
+        /*
+         * Sets the vessel description.
+         */
         void setVesselDescription(const std::string& description);
+
         //void setRoot(BasePart* pat);
+
+        /*
+         * Sets the player pointer, this means that the particular player is controlling this
+         * vessel.
+         *
+         * @player: pointer to the player object.
+         */
         void setPlayer(Player* player);
+
+        /*
+         * Sets the velocity vector of the vessel, use with caution.
+         *
+         * @velocity: new velocity value.
+         */
         void setVesselVelocity(const btVector3& velocity);
 
-        /* These functions should only be used in the editor */
+        /*
+         * Attaches a child to a parent node using its ID. Returns false if the parent ID is not
+         * found in the tree.
+         *
+         * @child: shared pointer of the child, the parent will take ownership of this pointer.
+         * @parent_id: id of the parent we want to attach the child to.
+         */
         bool addChildById(std::shared_ptr<BasePart>& child, std::uint32_t parent_id);
+
+        /*
+         * Nasty method that attaches a child to the referred parent pointer. I don't think this is
+         * used anywhere and to be hones I don't like it. The method uses the getSharedPtr to
+         * obtain the shared pointer. Returns false if the parent doesn't belong to the tree.
+         *
+         * @child: raw pointer to the child.
+         * @parent: raw pointer to the parent.
+         */
         bool addChild(BasePart* child, BasePart* parent);
+
+        /*
+         * Removes a child and returns its shared pointer. I don't like that we pass a raw pointer.
+         * removeChildById seems cleaner and more safe. Returns a shared pointer to null if the
+         * child is not found.
+         *
+         * @child: raw pointer to the child.
+         */
         std::shared_ptr<BasePart> removeChild(BasePart* child);
+
+        /*
+         * Removes a child and returns its shared pointer using its ID. Returns a shared pointer
+         * to null if the child is not found.
+         *
+         * @child_id: ID of the child.
+         */
         std::shared_ptr<BasePart> removeChildById(std::uint32_t child_id);
 
+        /*
+         * Method that should be called each time the tree updates. This should be mainly called
+         * by the vessel's nodes.
+         */
         void onTreeUpdate();
 
+        /*
+         * Returns a pointer to the root of the tree.
+         */
         BasePart* getRoot();
+
+        /*
+         * Returns a pointer to a node of the tree using its ID.
+         *
+         * @id: ID of the part.
+         */
         BasePart* getPartById(std::uint32_t id);
+
+        /*
+         * Const version of getRoot.
+         */
         const BasePart* getRoot() const;
+
+        /*
+         * Const version of getPartById.
+         */
         const BasePart* getPartById(std::uint32_t id) const;
+
+        /*
+         * Returns the name of the vessel.
+         */
         const std::string& getVesselName() const;
+
+        /*
+         * Returns the description of the vessel.
+         */
         const std::string& getVesselDescription() const;
+
+        /*
+         * Returns a reference to the list of parts of the vessel.
+         */
         std::vector<BasePart*>& getParts();
+
+        /*
+         * Const version of getParts.
+         */
         const std::vector<BasePart*>& getParts() const;
+
+        /*
+         * Returns the ID of the vessel.
+         */
         std::uint32_t getId() const;
+
+        /*
+         * Prints the vessel tree via stdout.
+         */
         void printVessel() const;
+
+        /*
+         * Returns the input pointer, I think it's used by the parts.
+         */
         const Input* getInput() const;
+
+        /*
+         * Returns the yaw input of the vessel.
+         */
         float getYaw() const;
+
+        /*
+         * Returns the pitch input of the vessel.
+         */
         float getPitch() const;
+
+        /*
+         * Returns the total mass of the vessel.
+         */
         double getTotalMass() const;
+
+        /*
+         * Returns the center of mass (global) of the vessel.
+         */
         const btVector3& getCoM() const;
+
+        /*
+         * Prints the staging of the vessel.
+         */
         void printStaging() const;
+
+        /*
+         * Returns some distance that is supposed to be I think the height between the vessel's
+         * root origin and it's lowest point.
+         */
         double getLowerBound() const;
 
+        /*
+         * Updates stuff like the mass and the subtree.
+         */
         void update();
+
+        /*
+         * Returns staging, input, subtree, etc.
+         */
         void updateCoM();
 };
 
